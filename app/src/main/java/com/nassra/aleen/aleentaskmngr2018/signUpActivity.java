1838e753fa4434418ee54705a25afbd244091219
user <muhammed.awad@outlook.com>
2018-10-17 08:57:03 +0300

    1.splash is ok 2.added mainActivity tabs 3.buliding login,and sign up

--- app/src/main/java/com/nassra/aleen/aleentaskmngr2018/signUpActivity.java
+++ app/src/main/java/com/nassra/aleen/aleentaskmngr2018/signUpActivity.java
@@ -1,20 +1,25 @@
 package com.nassra.aleen.aleentaskmngr2018;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class signUpActivity extends AppCompatActivity {
     private EditText etFirstName,etLastName,etPhone,etEmail2,etPassword2;
     private Button btnSave;
-    FirebaseUser user;
-    FirebaseAuth auth;
+    private FirebaseUser user;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +41,25 @@ public class signUpActivity extends AppCompatActivity {
         auth=FirebaseAuth.getInstance();
         user=auth.getCurrentUser();
 
+
     }
+    private void creatAcount(String email,String passw){
+        auth.createUserWithEmailAndPassword(email,passw).addOnCompleteListener(signUpActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(signUpActivity.this, "AUTHENTICTION SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                else {
+                    Toast.makeText(signUpActivity.this, "Authentication failed"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+                }
+                
+            }
+        });
+    }
+
 
 
 }
