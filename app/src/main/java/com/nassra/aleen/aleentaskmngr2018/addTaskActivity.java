3be277144c27c1882aee5244496ec433c9b8e40b
user <muhammed.awad@outlook.com>
2018-11-12 14:54:51 +0200

    1.splash is ok 2.added mainActivity tabs 3.buliding login,and sign up

--- app/src/main/java/com/nassra/aleen/aleentaskmngr2018/addTaskActivity.java
+++ app/src/main/java/com/nassra/aleen/aleentaskmngr2018/addTaskActivity.java
@@ -3,6 +3,7 @@ package com.nassra.aleen.aleentaskmngr2018;
 import android.app.DatePickerDialog;
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +12,13 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.nassra.aleen.aleentaskmngr2018.data.MyTask;
 
 import java.util.Calendar;
@@ -53,24 +60,49 @@ public class addTaskActivity extends AppCompatActivity {
         boolean isok = true;
         String title = etTitle.getText().toString();
         String task = etTask.getText().toString();
+        String date=etDate.getText().toString();
 
         String importent = tvImportent.getText().toString();
         String necessary = tvNecessary.getText().toString();
         int sImportent = skbrImportent.getProgress();
         int snecessary = skbrNecessary.getProgress();
         if (title.length() == 0) {
-            etTitle.setError("");
+            etTitle.setError("you have to write a title ");
+            isok=false;
 
 
         }
         if (task.length()==0){
-            etTask.setError("");
+            etTask.setError("yo have to write a task");
+            isok=false;
         }
         if (isok)
         {
             MyTask task1=new MyTask();
             task1.setCreatDate(new Date());
-            //task1.setDueDate();
+            task1.setDueDate(new Date(date));
+            task1.setTitle(title);
+            task1.setTesk(task);
+            task1.setImportent(sImportent);
+            task1.setNexessary(snecessary);
+            FirebaseAuth auth=FirebaseAuth.getInstance();
+            task1.setOwner(auth.getCurrentUser().getEmail());
+            DatabaseReference reference= FirebaseDatabase.getInstance().getReference();
+            String key=reference.child("my taskd").push().getKey();
+            task1.setKey(key);
+            reference.child("my tasks").child(key).setValue(task1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task1) {
+                    if (task1.isSuccessful()){
+                        Toast.makeText(addTaskActivity.this, "add successed", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(addTaskActivity.this, "add failed"+task1.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+
         }
     }
 
